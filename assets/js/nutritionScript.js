dbc522161f68fb9b1c60a8bd657cc87ecf15f4b6
nnamdionyeije <81874532+nnamdionyeije@users.noreply.github.com>
2023-03-06 20:34:33 -0500

    API and jQuery links
    Added some of the API functionality and some jQuery links

---
+++ assets/js/nutritionScript.js
@@ -0,0 +1,14 @@
+const settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch?cuisine=italian&diet=vegetarian&intolerances=gluten&minCalories=50&maxCalories=800",
+	"method": "GET",
+	"headers": {
+		"X-RapidAPI-Key": "0d7d04e9cemsh2554cf8f16e8e5bp103adejsn5ce9403882f6",
+		"X-RapidAPI-Host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
+	}
+};
+
+$.ajax(settings).done(function (response) {
+	console.log(response);
+});
