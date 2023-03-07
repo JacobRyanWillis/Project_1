ac69b6828613cab585c762f5f72c1f34a659e099
nnamdionyeije <81874532+nnamdionyeije@users.noreply.github.com>
2023-03-06 21:32:30 -0500

    Update nutritionScript.js
    Added the base of the JS functionality for the nutrition fields

--- assets/js/nutritionScript.js
+++ assets/js/nutritionScript.js
@@ -4,11 +4,30 @@
 
 // 
 
+var skillsSelect = document.getElementById("speed");
+var selectedTextDiet = skillsSelect.options[skillsSelect.selectedIndex].text;
+
+
+var intolleranceSelect = document.getElementById("files");
+var selectedTextIntollerance = intolleranceSelect.options[intolleranceSelect.selectedIndex].text;
+
+// var minCalories = $(".search-input").val();
+var minCalories = 200;
+// var maxCalories = $(".search-input2").val();
+var maxCalories = 900;
+console.log(minCalories);
+
+console.log(selectedTextIntollerance)
+
+//"https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch?diet=vegetarian&intolerances=gluten&minCalories=50&maxCalories=800"
+
+var spooge = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch?diet=" + selectedTextDiet + "&intolerances=" + selectedTextIntollerance + "&minCalories=" + minCalories + "&maxCalories=" + maxCalories;
+
 
 const settings = {
 	"async": true,
 	"crossDomain": true,
-	"url": "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch?cuisine=italian&diet=vegetarian&intolerances=gluten&minCalories=50&maxCalories=800",
+	"url": spooge,
 	"method": "GET",
 	"headers": {
 		"X-RapidAPI-Key": "0d7d04e9cemsh2554cf8f16e8e5bp103adejsn5ce9403882f6",
@@ -17,5 +36,21 @@ const settings = {
 };
 
 $.ajax(settings).done(function (response) {
-	console.log(response);
+	var currIndex = response.results[0].id;
+	console.log(currIndex);
+});
+
+const settings2 = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/479101/information",
+	"method": "GET",
+	"headers": {
+		"X-RapidAPI-Key": "0d7d04e9cemsh2554cf8f16e8e5bp103adejsn5ce9403882f6",
+		"X-RapidAPI-Host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
+	}
+};
+
+$.ajax(settings2).done(function (response2) {
+	console.log(response2);
 });
