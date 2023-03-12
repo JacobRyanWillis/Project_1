39aec43023be403669fd57ea97c3f842fe56c139
JakeNazty <jacobwillis2012@gmail.com>
2023-03-11 21:34:40 -0500

    'finished localStorage, favorites, and added html'

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 var fitnessBtn = $('#fitnessBtn');
 var nutritionBtn = $('#nutritionBtn');
 var exercises = [];
+var favorites = JSON.parse(localStorage.getItem('favorites')) || [];
 
 $(document).ready(function () {
   fitnessBtn.click(function () {
@@ -41,26 +42,34 @@ function randomWorkout(data) {
 }
 // Displays the workout: bodyPart, Gif, and name on the screen.
 function displayWorkout(workout) {
+  $(".card-content").empty();
   var gifUrl = workout.gifUrl;
   var name = workout.name;
-
-  // Creates Elements and Adds classes
-  var workoutDiv = $('.workout');
-  var h2 = $('<h2>').addClass('exerciseName');
+  var h2 = $('<h2>')
   var gifImg = $('<img>');
   var youTubeLink = $('<a>');
-  var bttnDiv = $('.nextFavorite');
-  // var favoriteBttn = $('<button>').addClass('column is-four-fifths favorite').text('Favorite this exercise.');
-  var favoriteBttn = $('#fitnessBtn1').text('Favorite this exercise');
-  // var nextBttn = $('<button>').addClass('column is-four-fifths nextExercise').text('Next Exercise!');
-  var nextBttn = $('#fitnessBtn2').text("Next Exercise!");
+  var workoutDiv = $("<div>").addClass("workout");
+  var cardDiv = $("<div>").addClass("card has-background-primary-light card-2");
+  var contentsDiv = $("<div>").addClass("card-contents");
+  var titleParagraph = $("<p>").addClass("title");
+  contentsDiv.append(titleParagraph);
+  var imageDiv = $("<div>").addClass("card-image");
+  var contentDiv = $("<div>").addClass("card-content");
+  var footerDiv = $("<footer>").addClass("card-footer");
+  var buttonContainer = $("<p>").addClass("card-footer-item");
+  var button1 = $("<button>").attr("id", "fitnessBtn1").addClass("card-btn card-btn2");
+  var button2 = $("<button>").attr("id", "fitnessBtn2").addClass("card-btn card-btn2");
+  var favoriteBttn = button1.text('Favorite this exercise');
+  var nextBttn = button2.text("Next Exercise!");
 
-  // Appends to the webpage and adds attributes
-  $('#workoutDiv').removeClass('is-hidden');
-  // workoutDiv.append(h2);
+  // Appends elements
+  buttonContainer.append(button1, button2);
+  footerDiv.append(buttonContainer);
+  cardDiv.append(contentsDiv, imageDiv, contentDiv, footerDiv);
+  workoutDiv.append(cardDiv);
+  $("#workoutDiv").removeClass('is-hidden').append(workoutDiv);
   $(".title").append(h2);
   h2.text(name);
-  // workoutDiv.append(gifImg);
   $(".card-image").append(gifImg);
   gifImg.attr('src', gifUrl);
   youTubeLink.text('Click me to watch a video of the exercise.');
@@ -68,19 +77,32 @@ function displayWorkout(workout) {
   youTubeLink.attr('target', '_blank');
   youTubeLink.css('font-size', '20px');
   youTubeLink.css('margin', '2rem');
-  // workoutDiv.append(youTubeLink);
   $(".card-content").append(youTubeLink);
-  // workoutDiv.append(bttnDiv);
-  // $(".card-btn").append(bttnDiv);
-  // bttnDiv.append(favoriteBttn);
-  // bttnDiv.append(nextBttn);
+
+  // Takes user back to choose another exercise
+  nextBttn.on('click', function() {
+    location.href = 'fitness.html';
+  })
+  // Stores exercise into local storage if they favorite the exercise.
+  favoriteBttn.on('click', function() {
+    var isFavorite = favorites.some(function(favorite) {
+      return favorite.name === workout.name;
+    });
+    if (isFavorite) {
+      alert('Exercise is already in favorites!');
+    } else {
+      favorites.push(workout);
+      localStorage.setItem('favorites', JSON.stringify(favorites));
+      alert('Exercise added to favorites!');
+    }
+  })
 }
 
 
 // Event listener for exercise buttons.
 $(document).ready(function () {
   var selectedBodyParts = [];
-  $('.card-btn').click(function () {
+  $('.exerciseBttns').click(function () {
     $('#hide-choices').hide();
     var bodyParts = $(this).data('body-part').split(',');
     selectedBodyParts.push(...bodyParts);
@@ -97,3 +119,13 @@ $(document).ready(function () {
   selectedBodyParts = [];
 });
 
+// Event listener for favorites.
+$(document).ready(function () { 
+  $('.favoritesBttn').on('click', function() {
+    $('#hide-choices').hide();
+    favorites.forEach(function(favorite) {
+      displayWorkout(favorite);
+    });
+  })  
+
+});
